933b12456acc01c7a38d48527c9e96faf6f1d260
Jonn Mostovoy <amarr.industrial@gmail.com>
2016-08-05 04:22:49 +0300

    Implement dealer

--- IO/dealer.py
+++ IO/dealer.py
@@ -1,35 +1,49 @@
 #!/usr/bin/env python3
 
 import pprint
-from random import randint
 from twisted.internet import protocol
 from twisted.internet import reactor
 
 class DealerServer(protocol.ProcessProtocol):
   def __init__(self):
-    self.id = randint(1, 10000)
-    self.state = { 'moves': [] }
+    self.moves = []
+    self.subscriptions = []
   def connectionMade(self):
-    pprint.pprint('connectionMade')
-    self.transport.write(b'u\n')
+    return
   def childDataReceived(self, fd, x): 
-    self.state['moves'].append((fd, x))
-    self.transport.write(b'd\n')
+    self.moves.append(x)
+    for ds in self.subscriptions:
+      ds.send(x)
   def childConnectionLost(self, fd):
     pprint.pprint(fd)
   def proecssExited(self, reason):
     pprint.pprint(reason)
   def processEnded(self, reason):
-    pprint.pprint(self.state)
+    pprint.pprint(self.moves)
+  def subscribe(self, ds):
+    self.subscriptions.append(ds)
+  def send(self, x):
+    self.transport.write(x)
 
 def main():
-  dealerServer = DealerServer()
-  reactor.spawnProcess(dealerServer,              # :: ProcessProtocol
-                       'player.py',               # :: Executable
-                       ['A', '-n 2', 'b'],        # :: Args
-                       childFDs={0: 'w',          # :: Map FD Perm
-                                 1: 'r',
-                                 2: 'r'})
+  def cpp(x):
+   return chr(ord(x) + 1) 
+  dealerServers = [ DealerServer()
+                  , DealerServer()
+                  , DealerServer() ]
+  for dsi in dealerServers:
+    for dsj in dealerServers:
+      if dsi != dsj:
+        dsi.subscribe(dsj)
+  strat = '`'
+  for dsk in dealerServers:
+    strat = cpp(strat)
+    reactor.spawnProcess(dsk,                       # :: ProcessProtocol
+                         'player.py',               # :: Executable
+                         ['A', '-n 3', cpp(strat)], # :: Args
+                         childFDs={0: 'w',          # :: Map FD Perm
+                                   1: 'r',
+                                   2: 'r'})
   reactor.run()
 
 if __name__ == '__main__':
